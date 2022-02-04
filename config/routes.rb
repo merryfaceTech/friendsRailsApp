bbfbb9d92d79a13da8aaae5f60868dc1e413026e
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-04 15:46:33 +0000

    basic navigation to friends pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :friends
+  
   # get 'home/index'
   root 'home#index'
   get 'home/about'
@@ -9,9 +10,6 @@ Rails.application.routes.draw do
 
 
 
-
-
-  
   #  ==================
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
